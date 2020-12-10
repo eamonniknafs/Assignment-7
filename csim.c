8b8d4309a929d7f4398610708fff014571a2bfc0
Eamon Niknafs <en@csa2.bu.edu>
2020-12-10 00:03:03 -0500

    Added header comment - Includes description of csim.c - Includes name and email

--- csim.c
+++ csim.c
@@ -1,3 +1,12 @@
+/*
+ * csim.c: a cache simulator that takes a valgrind memory trace
+ * as input, simulates the hit/miss behavior of a cache memory
+ * on this trace, and outputs the total number of hits, misses,
+ * and evictions.
+ *
+ * Name: Eamon Niknafs
+ * Email: en@bu.edu
+ */
 #include "cachelab.h"
 
 int main()
