4c1239acbc99bb42f17bef057194c3637cfd63f3
Eamon Niknafs <en@csa2.bu.edu>
2020-12-10 01:15:16 -0500

    Added type definitions - Added type for each line - Added type for each set - Added type for stats, including an LRU counter - Added type for each cache, including sets and stats

--- csim.c
+++ csim.c
@@ -9,6 +9,30 @@
  */
 #include "cachelab.h"
 
+/* Define type for cache line */
+typedef struct {
+    short valid;
+    unsigned long long tag;
+    unsigned long long block;
+} line_t;
+
+/* Define type for cache set */
+typedef line_t* set_t;
+
+/* Define type for cache statistics */
+typedef struct{
+    unsigned int hits = 0;
+    unsigned int misses = 0;
+    unsigned int evictions = 0;
+    unsigned long long int lru_count = 1;
+} stats_t;
+
+/* Define type for entire cache */
+typedef struct{
+    set_t sets;
+    stats_t stats;
+} cache_t;
+
 int main()
 {
     printSummary(0, 0, 0);
